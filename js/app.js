66a182c14316ae2453375edf24842e8b943825b7
victorgomesinfor <victor.gomes990@cs.cruzeirodosul.edu.br>
2026-09-06 16:57:23 -0300

    Adiciona diálogo para entrada de nome e estilização de componentes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 
+const button = document.getElementById('btn-start');
+const modal = document.getElementById('dialog-name');
+const close = document.getElementById('close');
+
+button.onclick = function() {
+    modal.showModal();
+}
+
+close.onclick = function() {
+    modal.close();
+}
