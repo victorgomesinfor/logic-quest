498e1f9a64fa7034ebae92ae706e2399f6c5c930
victorgomesinfor <victor.gomes990@cs.cruzeirodosul.edu.br>
2026-09-08 20:44:56 -0300

    Adiciona lógica para captura do nome do jogador e exibe mensagem de boas-vindas

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 const button = document.getElementById('btn-start');
 const modal = document.getElementById('dialog-name');
 const close = document.getElementById('close');
+const form = document.getElementById('form');
+const submit = document.getElementById('input');
 
 //abre janela modal
 button.onclick = function() {
@@ -13,10 +15,25 @@ close.onclick = function() {
     modal.close();
 }
 
-//Pega o nome do jogador e exibe uma mensagem de boas-vindas.
-enviar.onclick = function(){
-    const button = document.getElementById('enviar');
-    let name = document.getElementById('input').value;
+//Captura o nome inputado e cria o player, depois mostra um alert mostrando que tudo deu certo.
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = submit.value;
+
+    if(name.trim() === ''){
+        alert ('Digite seu nome!')
+        return
+    }
+
+    const player = {
+        name: name,
+        level: 1,
+        xp: 0,
+        score: 0
+    };
+
+    localStorage.setItem('player', JSON.stringify(player));
     alert("Bem-vindo ao Logic Quest!" + " " + name + " vamos começar nossa aventura!");
 
-}
+});
